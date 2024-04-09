1f0d8fb6367e7279a5ce39c7d46aa76a381bdb25
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-04-09 17:32:55 +0530

    check number power of two

---
+++ day14_isPowerOfTwo.js
@@ -0,0 +1,12 @@
+let num=16;
+function checkPower(num){
+    let power=false;
+    for(let i=1;i<100;i++){
+        if(num===Math.pow(2,i)){
+            power= true;
+          break;
+        }
+    }
+    return power;
+}
+console.log(checkPower(num))
