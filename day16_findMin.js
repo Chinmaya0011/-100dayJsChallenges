e6afc9f971a7cbab5650cbee5aeccc4d928bbc17
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-04-11 19:28:04 +0530

    find minimum

--- day16_findMin.js
+++ day16_findMin.js
@@ -1,13 +1,21 @@
 let arr=[12,43,23,454,23,632,56,332,42];
 
 function findMin(arr){
-    let minNum=arr[0];
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]<minNum){
-            minNum=arr[i];
+    // let minNum=arr[0];
+    // for(let i=0;i<arr.length;i++){
+    //     if(arr[i]<minNum){
+    //         minNum=arr[i];
+    //     }
+    // }
+    // return minNum;
+    let smal=0;
+  arr.reduce((a,b)=>{
+        if(a<b){
+           smal=a;
+
         }
-    }
-    return minNum;
+    });
+    return smal;
    
 }
 console.log(findMin(arr))
