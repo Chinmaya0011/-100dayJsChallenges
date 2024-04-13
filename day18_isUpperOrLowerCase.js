9c270febb079548460a78669af4f38cf35f49980
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-04-13 19:47:34 +0530

    check the charcter lowerOrUpper case

---
+++ day18_isUpperOrLowerCase.js
@@ -0,0 +1,13 @@
+let char='c';
+
+function check(char){
+    let char1=char.toUpperCase()
+if(char===char1){
+    console.log('it is uppercase')
+}
+else{
+    console.log('it is lowercase')
+}
+console.log(char===char1)
+}
+check(char)
