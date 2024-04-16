f8c773b0f1deaff7de6b818306e241b3012474e0
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-04-16 23:26:52 +0530

    find average and medium number

--- day18_isUpperOrLowerCase.js
+++ day18_isUpperOrLowerCase.js
@@ -1,13 +1,17 @@
-let char='c';
+// let char='c';
 
-function check(char){
-    let char1=char.toUpperCase()
-if(char===char1){
-    console.log('it is uppercase')
-}
-else{
-    console.log('it is lowercase')
-}
-console.log(char===char1)
-}
-check(char)
+// function check(char){
+//     let char1=char.toUpperCase()
+// if(char===char1){
+//     console.log('it is uppercase')
+// }
+// else{
+//     console.log('it is lowercase')
+// }
+// console.log(char===char1)
+// }
+// check(char)
+
+let str="saheed laxman nayak medical college & hospital koraput";
+
+console.log(str.toUpperCase())
---
+++ day21_findMedium.js
@@ -0,0 +1,18 @@
+// let arr=[1,2,3,4,5];
+
+// function findMedium(arr){
+//     let med=Math.floor(arr.length/2);
+//     return arr[med];
+
+// }
+// console.log(findMedium(arr))
+let arr=[12,24,4,2,45623,23,456];
+
+function findMedium(arr){
+    let sum=0;
+    for(let i=0;i<arr.length;i++){
+        sum+=arr[i];
+    }
+    return sum/arr.length;
+}
+console.log(findMedium(arr))
