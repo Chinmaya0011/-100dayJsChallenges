af93e90e6fa64051d9ec8f14914ab185eff7cdcd
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-04-22 20:05:30 +0530

    fibonacci recursions

---
+++ day26_binaryToNumber.js
@@ -0,0 +1,12 @@
+let binary=101;
+
+function toNumber(binary){
+    let str=binary.toString();
+    let sum=0;
+    for(let i=0;i<str.length;i++){
+        sum+=str[i]*2**(str.length-1-i);
+    }
+    return sum;
+}
+
+console.log(toNumber(binary))
