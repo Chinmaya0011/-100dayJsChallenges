7b5a486edccd3657fceee517cb14a9141ae4651d
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-04-23 17:09:46 +0530

    update

--- day27_repeatString.js
+++ day27_repeatString.js
@@ -1,11 +1,59 @@
-let string='chinu';
+// Define the question
+ question = "Repeat the given string a specified number of times.";
 
-function repeatString(string,timeOfRepeat){
-    let repeatedString="";
-    for(let i=0;i<=timeOfRepeat;i++){
-        repeatedString+=string;
+// Define the string
+let string = 'chinu';
+
+// Define the function to repeat the string
+function repeatString(string, timeOfRepeat) {
+    let repeatedString = "";
+    for (let i = 0; i <= timeOfRepeat; i++) {
+        repeatedString += string;
     }
 
     return repeatedString;
 }
-console.log(repeatString(string,5))
+
+// Define the number of times to repeat the string
+let timeOfRepeat = 5;
+
+// Save the code to a variable
+selectedFileCode = `
+// Define the question
+let question = "${question}";
+
+// Define the string
+let string = '${string}';
+
+// Define the function to repeat the string
+function repeatString(string, timeOfRepeat) {
+    let repeatedString = "";
+    for (let i = 0; i <= timeOfRepeat; i++) {
+        repeatedString += string;
+    }
+
+    return repeatedString;
+}
+
+// Define the number of times to repeat the string
+let timeOfRepeat = ${timeOfRepeat};
+
+// Output the result
+console.log(repeatString(string, timeOfRepeat));
+`;
+
+// Execute the function and log the result
+console.log(repeatString(string, timeOfRepeat));
+
+// Display the question and execute the function if running in a browser
+if (typeof document !== 'undefined') {
+    // Display the question
+    document.getElementById('question').innerHTML = question;
+
+    // Execute the function
+    let result = repeatString(string, timeOfRepeat);
+    document.getElementById('output').innerHTML = `The repeated string for ${string} ${timeOfRepeat} times is: ${result}`;
+} else {
+    // Log the result if not running in a browser
+    console.log(repeatString(string, timeOfRepeat));
+}
--- day28_truncateString.js
+++ day28_truncateString.js
@@ -1,8 +1,49 @@
-let str="chinmaya kumar mishra";
-let num=5;
-function turncateString(str,num){
+// Define the question
+question = "Truncate a given string to a specified number of characters.";
 
-    return str.slice(0,num)+"...";
+// Define the string
+let str = "chinmaya kumar mishra";
+
+// Define the number of characters to truncate the string
+let num = 5;
+
+// Define the function to truncate the string
+function truncateString(str, num) {
+    return str.slice(0, num) + "...";
 }
 
-console.log(turncateString(str,num))
+// Save the code to a variable
+selectedFileCode = `
+// Define the question
+let question = "${question}";
+
+// Define the string
+let str = "${str}";
+
+// Define the number of characters to truncate the string
+let num = ${num};
+
+// Define the function to truncate the string
+function truncateString(str, num) {
+    return str.slice(0, num) + "...";
+}
+
+// Output the result
+console.log(truncateString(str, num));
+`;
+
+// Execute the function and log the result
+console.log(truncateString(str, num));
+
+// Display the question and execute the function if running in a browser
+if (typeof document !== 'undefined') {
+    // Display the question
+    document.getElementById('question').innerHTML = question;
+
+    // Execute the function
+    let result = truncateString(str, num);
+    document.getElementById('output').innerHTML = `The truncated string for "${str}" to ${num} characters is: ${result}`;
+} else {
+    // Log the result if not running in a browser
+    console.log(truncateString(str, num));
+}
