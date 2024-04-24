fffb6caadd0c2ff7bcff0251875b1512f027e846
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-04-24 22:01:43 +0530

    day 29 print number in

---
+++ day29_printNumberBetween.js
@@ -0,0 +1,12 @@
+let startNumber=423;
+let endingNumber=505;
+
+function printNumber(startNumber,endingNumber){
+    let arr=[];
+    for(let i=startNumber;i<=endingNumber;i++){
+       arr.push(i);
+    }
+    console.log(arr)
+}
+
+printNumber(startNumber,endingNumber)
