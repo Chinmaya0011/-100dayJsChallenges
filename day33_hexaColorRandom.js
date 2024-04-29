f0437605a64a6835aab8a611b16ee1f700990fb8
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-04-29 23:59:22 +0530

    update

--- day33_hexaColorRandom.js
+++ day33_hexaColorRandom.js
@@ -1,13 +1,21 @@
-function generateSixDigitCode() {
-    const characters = '0123456789abcdefghijklmnopqrstuvwxyz';
-    let code = '';
-    for (let i = 0; i < 6; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        code += characters[randomIndex];
-    }
-    return "#"+code;
-}
-
-// Example usage
-const sixDigitCode = generateSixDigitCode();
-console.log(sixDigitCode);
+function generateRandomColor() {
+    // Generate random R, G, and B values
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+  
+    // Convert decimal to hexadecimal
+    var hexR = r.toString(16).padStart(2, '0');
+    var hexG = g.toString(16).padStart(2, '0');
+    var hexB = b.toString(16).padStart(2, '0');
+  
+    // Concatenate the values to form a hexadecimal color code
+    var hexColor = '#' + hexR + hexG + hexB;
+  
+    return hexColor;
+  }
+  
+  // Example usage:
+  var randomColor = generateRandomColor();
+  console.log(randomColor);
+  
