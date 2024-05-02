0291d3ef7b445dc9bea5424596dfe787c026caa6
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-05-02 23:19:17 +0530

    day 36 objectToArray arrayToObject

--- day29_printNumberBetween.js
+++ day29_printNumberBetween.js
@@ -1,12 +1,34 @@
-let startNumber=423;
-let endingNumber=505;
+// let startNumber=423;
+// let endingNumber=505;
 
-function printNumber(startNumber,endingNumber){
-    let arr=[];
-    for(let i=startNumber;i<=endingNumber;i++){
-       arr.push(i);
-    }
-    console.log(arr)
-}
+// function printNumber(startNumber,endingNumber){
+//     let arr=[];
+//     for(let i=startNumber;i<=endingNumber;i++){
+//        arr.push(i);
+//     }
+//     console.log(arr)
+// }
 
-printNumber(startNumber,endingNumber)
+// printNumber(startNumber,endingNumber)
+// function strCount(str, letter){  
+//  let count=0;
+//  for(let i=0;i<str.length;i++){
+//     if(str[i]===letter){
+//         count++
+//     }
+//  }
+//  return count;
+//   }
+
+function reverseLetter(str) {
+    let str1="";
+     for(let i=str.length;i>0;i--){
+       if(/a-z/.test(str[i])){
+         str1+=str[i]
+       }
+     }
+     return str1;
+   }
+   console.log(reverseLetter("chinu13xa"))
+
+//   console.log(strCount("chinmaya","p"))
---
+++ day36_objectToArray.js
@@ -0,0 +1,9 @@
+let obj={
+    name:"chinmaya",
+    age:21,
+
+}
+let entries=Object.entries(obj);
+console.log(entries)
+let fromEntries=Object.fromEntries(entries);
+console.log(fromEntries)
