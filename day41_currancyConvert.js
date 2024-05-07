dc67deaf1be74288c204fd8f933506dd909df5ae
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-05-07 18:18:18 +0530

    convert currancy

---
+++ day41_currancyConvert.js
@@ -0,0 +1,24 @@
+let currency = {
+    "usd": 1,
+    "euro": 0.9,
+    "gbp": 0.7,
+    "inr": 82,
+};
+
+function currencyConvert(baseAmount, cur1, cur2) {
+let amountInUsd=0;
+  for(let i in currency){
+    if(i===cur1){
+amountInUsd=baseAmount/currency[i]
+    }
+  }
+let convertAmount=0;
+for(let i in currency){
+    if(i===cur2){
+convertAmount=amountInUsd*currency[i];
+    }
+  }
+  return convertAmount;
+}
+
+console.log(currencyConvert(100, "euro", "inr")); // Output should be 100 / 82
