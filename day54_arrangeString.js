c8ac4c81f1a5b49d8e961be2044b4a6487f5cd54
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2024-05-26 12:29:32 +0530

    arrange a string number wise

---
+++ day54_arrangeString.js
@@ -0,0 +1,13 @@
+function order(words){
+    let words1=words.split(" ")
+let result=[];
+for(let i=1;i<=words1.length;i++){
+for(let j=0;j<words1.length;j++){
+    if(words1[j].includes(i)){
+        result.push(words1[j])
+    }
+}
+}
+return result.join(" ");
+}
+  console.log(order("is2 Thi1s T4est 3a"))
